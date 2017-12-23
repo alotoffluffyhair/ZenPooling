d158008fe017c2454df3743622a2bc8c0310510d
Scott Griffiths <Scott@Scotts-MBP.home>
2017-12-23 22:37:07 +0000

    Restructured filing, added package.json

--- init.js
+++ init.js
@@ -1,7 +1,10 @@
 var fs = require('fs');
 var cluster = require('cluster');
-var Logger = require('./logger.js');
+
+var Logger = require('./src/logger.js');
+
 JSON.minify = require('node-json-minify');
+
 
 if (!fs.existsSync('config.json')){
     console.log('config.json file is missing please create. Program quitting.');
@@ -10,11 +13,18 @@ if (!fs.existsSync('config.json')){
 
 var serverConfig = JSON.parse(JSON.minify(fs.readFileSync("config.json", {encoding: 'utf8'})));
 var logger = new Logger(serverConfig.logLevel);
-
-if (cluster.isMaster) {
-  logger.special('Master', 'Cluster setup', 'Main thread talking');
-  cluster.fork();
-  cluster.fork();
-} else if (cluster.isWorker) {
-  console.log(`I am worker #${cluster.worker.id}`);
+  	
+if (cluster.isWorker) {
+	console.log(`I am worker ${cluster.worker.id}`);
+	logger.all('Worker', '${cluster.worker.id}', 'test');
+	return;
 }
+
+(function init() {
+	logger.special("Master", "Init setup", "Setting up");
+	cluster.fork();
+	cluster.fork();
+	cluster.fork();
+	cluster.fork();
+	
+})();
---
+++ src/logger.js
@@ -0,0 +1,52 @@
+var colors = require("color");
+var dateFormat = require('dateformat');
+
+var loggingLevels = {
+	"all": 1,
+	"special": 2,
+	"warning": 3,
+	"error": 4
+};
+
+//Provides colour coding of the text based on the logging level of the message.
+var colorText = function(logLevel, text) {
+	switch(logLevel) {
+		case "all":
+			return text.green;
+		case "special":
+			return text.cyan;
+		case "warning":
+			return text.yellow;
+		case "error":
+			return text.red;
+		default:
+			console.log("Unknown logging level: " + logLevel);
+			return text;
+	}	
+};
+
+var Logger = function (loglvl) {
+	
+	var loggingLevelInt = loggingLevels[loglvl];
+	
+	var log = function(severity, system, component, text) {
+		if (loggingLevels[severity] < loggingLevelInt) return;
+		var entryDesc = dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss') + ' [' + system + ']\t';
+		enteryDesc = colorText(severity, entryDesc);
+		var logString = entryDesc + (' [' + component + ']\t') + text;
+		
+		console.log(logString);
+
+	};
+	
+	var _this = this;
+    Object.keys(loggingLevels).forEach(function(logType){
+        _this[logType] = function(){
+            var args = Array.prototype.slice.call(arguments, 0);
+            args.unshift(logType);
+            log.apply(this, args);
+        };
+    });
+	
+};
+module.exports = Logger;
